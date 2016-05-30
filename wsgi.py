68c0286417c6b35923482c30ad187d62caf07a11
Varunendra Pratap Singh <vpsingh016@gmail.com>
2016-05-30 14:11:49 +0530

    added wsgi and Procfile

---
+++ wsgi.py
@@ -0,0 +1,7 @@
+import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "thinkster_django_angular_boilerplate.settings")
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
